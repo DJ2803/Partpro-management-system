ca09d28f382f7a10059c9cf16b06996e688ba0f7
Sai Rahul <rahul.padma@thegisolutions.com>
2024-01-26 13:46:35 -0600

    Initial Changes for back end codebase

---
+++ src/main/java/com/unt/se/ppms/application/PartpromanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.unt.se.ppms.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartpromanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartpromanagementsystemApplication.class, args);
+	}
+
+}
