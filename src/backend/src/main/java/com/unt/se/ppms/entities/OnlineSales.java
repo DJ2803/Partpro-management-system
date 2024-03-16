2f849d976083262e55c3836419036f79cfcd160e
SnehaReddy13 <snehareddy21999@gmail.com>
2024-03-16 20:13:54 +0530

    added dashboard related entities and repositories

---
+++ src/backend/src/main/java/com/unt/se/ppms/entities/OnlineSales.java
@@ -0,0 +1,41 @@
+package com.unt.se.ppms.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "online_sales")
+public class OnlineSales {
+	
+	@Id
+	@Column(name = "sale_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long saleId;
+	
+	@Column(name = "total_price")
+	private float totalPrice;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "employee_id")
+    private Employee employer;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Products products;
+
+}
