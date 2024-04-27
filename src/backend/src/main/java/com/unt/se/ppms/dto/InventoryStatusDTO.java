0135aa0b5658d3c66350ffea29b11ccb31b33af6
SaiSamyukthaPaspuleti <SaiSamyukthaPaspuleti@my.unt.edu>
2024-04-27 10:19:00 -0500

    DTO changes for online sales and feedback

---
+++ src/backend/src/main/java/com/unt/se/ppms/dto/InventoryStatusDTO.java
@@ -0,0 +1,47 @@
+package com.unt.se.ppms.dto;
+
+public class InventoryStatusDTO {
+
+	private boolean status;
+	private long productId;
+	private String productName;
+	private float productPrice;
+	private String productDescription;
+	private String productImage;
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	public long getProductId() {
+		return productId;
+	}
+	public void setProductId(long productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public float getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(float productPrice) {
+		this.productPrice = productPrice;
+	}
+	public String getProductDescription() {
+		return productDescription;
+	}
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+	public String getProductImage() {
+		return productImage;
+	}
+	public void setProductImage(String productImage) {
+		this.productImage = productImage;
+	}
+}
